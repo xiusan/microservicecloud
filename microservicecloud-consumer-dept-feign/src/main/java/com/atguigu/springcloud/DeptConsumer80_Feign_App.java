b26d307567ebc33495f8dfa4206e4f4abfe2134b
xiusan <xiaojinlu1990@163.com>
2019-05-03 21:32:45 +0800

    脑图

--- microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @EnableFeignClients(basePackages= {"com.atguigu.springcloud"})
 @ComponentScan("com.atguigu.springcloud")
-public class DeptConsumer80_Feign_App {
-
-	public static void main(String[] args) {
+public class DeptConsumer80_Feign_App
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DeptConsumer80_Feign_App.class, args);
 	}
-
 }
